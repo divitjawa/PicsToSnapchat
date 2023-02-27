09f63a7b78d1c2c788752bfa694b5cc243ef0349
divitjawa <divit.dj@hotmail.com>
2023-02-27 00:12:53 -0800

    changes to share

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,31 +1,13 @@
 import React from 'react';
 import {StyleSheet, Image, Text, View, TouchableOpacity} from 'react-native';
-import {CreativeKit} from '@snapchat/snap-kit-react-native';
 import RNFetchBlob from 'rn-fetch-blob';
+import Share from 'react-native-share';
 
 const Picture = ({item}) => {
-
   return (
     <TouchableOpacity
       style={styles.picture}
-      onPress={() => {
-        const photoUrl = item.download_url;
-        console.log('photourl', photoUrl)
-
-        if (isValidHttpUrl(photoUrl)) {
-          RNFetchBlob.config({
-            fileCache: true,
-            appendExt: 'png',
-          })
-            .fetch('GET', photoUrl)
-            .then(res => {
-                console.log('res', res)
-                shareImageToSnapchat(`file://${res.data}`);
-            });
-        } else {
-            shareImageToSnapchat(photoUrl);
-        }
-      }}>
+      onPress={() => shareImageToSnapchat(item.download_url)}>
       <Image source={{uri: item.download_url}} style={styles.image} />
       <View style={styles.info}>
         <Text style={styles.title}>Author: {item.author}</Text>
@@ -45,21 +27,36 @@ const isValidHttpUrl = url => {
   return url.startsWith('https:') || url.startsWith('http:');
 };
 
-const shareImageToSnapchat = url => {
+const shareImageToSnapchat = photoUrl => {
+  console.log('url', photoUrl);
 
-  const photoContent = {
-    content: {
-      uri: url,
-    },
-    caption: 'caption string',
-    attachmentUrl: '',
-  };
-  CreativeKit.sharePhoto(photoContent)
-    .then(() => {
-      console.log('shared');
+  let imagePath = null;
+
+  const fs = RNFetchBlob.fs;
+  RNFetchBlob.config({
+    fileCache: true,
+  })
+    .fetch('GET', 'https://fastly.picsum.photos/id/6/5000/3333.jpg?hmac=pq9FRpg2xkAQ7J9JTrBtyFcp9-qvlu8ycAi7bUHlL7I')
+    // the image is now dowloaded to device's storage
+    .then(resp => {
+      // the image path you can use it directly with Image component
+      imagePath = resp.path();
+      return resp.readFile('base64');
     })
-    .catch(error => {
-      console.log('error', error);
+    .then(async base64Data => {
+      // here's base64 encoded image
+      // var base64Data = `data:image/jpg;base64,` + base64Data;
+      // imagePath = base64Data;
+      try {
+        const shareResponse = await Share.open({
+          message: 'test',
+          url: 'file://' + imagePath,
+        });
+      } catch (error) {
+        console.log('error', error);
+      }
+      // remove the file from storage
+      return RNFetchBlob.fs.unlink(imagePath);
     });
 };
 
